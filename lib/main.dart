73412dfbcc3b120f4716cecafc2f7d413ab1a79e
mostafa esmail <mostafaelshrbeeny98@gmail.com>
2024-09-02 19:49:54 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:e_commrece/core/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(
+      child: MaterialApp.router(
+        routerConfig: AppRouter.routes,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
