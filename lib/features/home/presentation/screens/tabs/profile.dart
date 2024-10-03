d772e96fae3d03c01a9eb78bcc45d7417916f4d7
mostafa esmail <mostafaelshrbeeny98@gmail.com>
2024-10-03 18:21:52 +0300

    finish profile tab

--- lib/features/home/presentation/screens/tabs/profile.dart
+++ lib/features/home/presentation/screens/tabs/profile.dart
@@ -1,10 +1,46 @@
+import 'package:e_commrece/core/utils/app_styles.dart';
+import 'package:e_commrece/core/utils/custom_space_height.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../core/utils/k_colors.dart';
+import '../../../../../generated/assets.dart';
+import '../../widgets/custom_field_of_profile.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("profile");
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.asset(
+              Assets.assetsImagesLogo,
+              width: 120,
+              colorFilter: const ColorFilter.mode(mainColor, BlendMode.srcIn),
+            ),
+            Text(
+              "Welcome, Mostafa",
+              style: AppStyles.textMedium20(context),
+            ),
+            const CustomSpaceHeight(height: .001),
+            Text(
+              "mostata98@gmail.com",
+              style: AppStyles.textMedium14(context),
+            ),
+            const CustomSpaceHeight(height: .04),
+            const CustomFieldOfProfile(title: 'Your full name', content: 'Mostafa Esmail',),
+            const CustomFieldOfProfile(title: 'Your E-mail', content: 'mostafa98@gmail.com',),
+            const CustomFieldOfProfile(title: 'Your password', content: '*****************',),
+            const CustomFieldOfProfile(title: 'Your mobile number', content: '01101340325',),
+            const CustomFieldOfProfile(title: 'Your Address', content: '6th October, street 11.....',),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/home/presentation/widgets/custom_field_of_profile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../../../core/utils/app_styles.dart';
+import '../../../../core/utils/custom_space_height.dart';
+import '../../../../core/utils/k_colors.dart';
+
+class CustomFieldOfProfile extends StatelessWidget {
+  const CustomFieldOfProfile({
+    super.key, required this.title, required this.content,
+  });
+  final String title ;
+  final String content ;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: AppStyles.textMedium20(context),
+        ),
+        const CustomSpaceHeight(height: .01),
+        Container(
+          height: 60,
+          width: double.maxFinite,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: strokColor)),
+          child: ListTile(
+            title: Text(
+              content,
+              style: AppStyles.textMedium14(context),
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: const Icon(Icons.edit),
+          ),
+        ),
+        const CustomSpaceHeight(height: .03)
+      ],
+    );
+  }
+}
