daf6d8f68fe048a98ed098a9ef9dd4c3ebf868ba
mostafa esmail <mostafaelshrbeeny98@gmail.com>
2024-10-19 14:17:19 +0300

    finish project

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -5,7 +5,6 @@ class Assets {
   static const String assetsImagesLogo = 'assets/images/logo.svg';
   static const String imagesAdd = 'assets/images/add.svg';
   static const String imagesCart = 'assets/images/cart.svg';
-  static const String imagesCat = 'assets/images/cat.png';
   static const String imagesCategories = 'assets/images/categories.svg';
   static const String imagesDelete = 'assets/images/delete.svg';
   static const String imagesFavIcon = 'assets/images/favIcon.svg';
@@ -18,11 +17,8 @@ class Assets {
   static const String imagesLogo = 'assets/images/logo.png';
   static const String imagesMinus = 'assets/images/minus.svg';
   static const String imagesPlus = 'assets/images/plus.svg';
-  static const String imagesProduct = 'assets/images/product.png';
   static const String imagesProfile = 'assets/images/profile.svg';
   static const String imagesSearch = 'assets/images/search.svg';
   static const String imagesStar = 'assets/images/star.svg';
-  static const String imagesSubcat = 'assets/images/subcat.png';
-  static const String imagesTest = 'assets/images/test.png';
 
 }
